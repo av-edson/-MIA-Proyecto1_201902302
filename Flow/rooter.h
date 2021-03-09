fcfd2a3660a37bb2b6b576b0201e25b58e4b36fd
Edson Avila <esavilaortiz@gmail.com>
2021-03-09 01:12:06 -0600

    En clion

--- Flow/rooter.h
+++ Flow/rooter.h
@@ -5,8 +5,6 @@
 #include <string>
 #include <array>
 #include "../Modules/diskModule.h"
-#include "../Modules/fileModule.h"
-#include "../Modules/ussrGrpModule.h"
 
 
 void readArguments(std::array<std::string, 11> lista);
---
+++ cmake-build-debug/CMakeFiles/Proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto.dir/Analizer/parser.cpp.o"
+  "CMakeFiles/Proyecto.dir/Analizer/scanner.cpp.o"
+  "CMakeFiles/Proyecto.dir/Flow/rooter.cpp.o"
+  "CMakeFiles/Proyecto.dir/Modules/diskModule.cpp.o"
+  "CMakeFiles/Proyecto.dir/main.cpp.o"
+  "Proyecto"
+  "Proyecto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
